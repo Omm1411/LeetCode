397307958d06d1d64d40cf4f3fc85c0914311802
Omm1411 <76055221+Omm1411@users.noreply.github.com>
2026-08-04 17:45:37 +0530

    Time: 113 ms (8.7%), Space: 155.1 MB (5.18%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> mpp;
-        mpp[0] = -1;
-        int prefix = 0;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            prefix += nums[i];
-            int remainder = prefix % k;
-            if (remainder < 0) remainder += k;
-
-            if (mpp.find(remainder) != mpp.end()) {
-                if (i - mpp[remainder] >= 2) return true;
-            } else {
-                mpp[remainder] = i;
+        int prefix=0;
+        int n=nums.size();
+        vector<int> rem(n,0);
+        bool status = false;
+        unordered_map<int,int> mpp; 
+        mpp[0]=-1;
+        for(int i=0;i<n;i++)
+        {
+            prefix+=nums[i];
+            int remainder = prefix%k;
+            rem[i]=remainder;
+            if(mpp.find(remainder)==mpp.end())
+            mpp[remainder]=i;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(mpp.find(rem[i])!=mpp.end() or mpp.find(0)!=mpp.end())
+            {
+                if(i-mpp[rem[i]]>=2)
+                {
+                    status=true;
+                }
             }
         }
-        return false;
+
+       return status; 
     }
 };
