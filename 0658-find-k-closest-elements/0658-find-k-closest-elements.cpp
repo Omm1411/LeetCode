9163deace58b7815a18aee809b653c28b52660dd
Omm1411 <76055221+Omm1411@users.noreply.github.com>
2026-08-09 18:49:11 +0530

    Time: 0 ms (100%), Space: 35.9 MB (81.26%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int l=0;
         int r = arr.size()-1;
-        while((r-l)>=k)
+        while((r-l)>=k)           // Check whether the window is less than k 
         {
-            if(x-arr[l]<=arr[r]-x)
+            if(x-arr[l]<=arr[r]-x)     // check whether left window is closer than right 
             {
-                r--;
+                r--;                    // move towards left
             }
             else
             {
-                l++;
+                l++;                    // else move towards right
             }
-        }
+        }                                  // loop ends when window is of k length
         return vector<int>(begin(arr)+l,begin(arr)+r+1);
         
     }
