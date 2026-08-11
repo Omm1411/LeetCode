9108f23cf0c2539deaa22b4824fae11e4b6690b2
Omm1411 <76055221+Omm1411@users.noreply.github.com>
2026-08-11 17:59:48 +0530

    Time: 0 ms (100%), Space: 17.6 MB (19.89%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,42 +1,47 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans = {-1, -1};
-
-        int start = 0, end = nums.size() - 1;
-
- 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                ans[0] = mid;
-                end = mid - 1;
+        vector<int> ans = {-1,-1};
+        int start = 0;
+        int x,y;x=-1;y=-1;
+        int end= nums.size()-1;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target)
+            {
+                ans[0]= mid;
+                end = mid-1;
             }
-            else if (nums[mid] < target) {
-                start = mid + 1;
+            else if(nums[mid]>target)
+            {
+                end = mid-1 ;
             }
-            else {
-                end = mid - 1;
+            else 
+            {
+                start = mid+1;
             }
+
         }
-
         start = 0;
-        end = nums.size() - 1;
+        end = nums.size()-1;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target)
+            {
+                ans[1]= mid;
+                start = mid+1;
+            }
+            else if(nums[mid]>target)
+            {
+                end = mid-1 ;
+            }
+            else 
+            {
+                start = mid+1;
+            }
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (nums[mid] == target) {
-                ans[1] = mid;
-                start = mid + 1;
-            }
-            else if (nums[mid] < target) {
-                start = mid + 1;
-            }
-            else {
-                end = mid - 1;
-            }
         }
 
         return ans;
