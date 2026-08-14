476d38af1c1012168e95dc76436d1d3706caac18
Omm1411 <76055221+Omm1411@users.noreply.github.com>
2026-08-14 20:31:14 +0530

    Time: 0 ms (100%), Space: 9.5 MB (80.36%) - LeetHub

--- 1276-number-of-burgers-with-no-waste-of-ingredients/1276-number-of-burgers-with-no-waste-of-ingredients.cpp
+++ 1276-number-of-burgers-with-no-waste-of-ingredients/1276-number-of-burgers-with-no-waste-of-ingredients.cpp
@@ -1,33 +1,10 @@
 class Solution {
 public:
     vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
-        vector<int> ans;
-        if(tomatoSlices%2!=0)return ans;
-        if(cheeseSlices>tomatoSlices)return ans;
-        int jumbo = 0;
-        int small = 0;
-        if(tomatoSlices>=2 and cheeseSlices > tomatoSlices/4)
-        {
-            while(tomatoSlices/4 < cheeseSlices)
-            {
-                small+=1;
-                tomatoSlices -= 2;
-                cheeseSlices -= 1;
-            }
-        } 
-        while(tomatoSlices %4 == 0 and tomatoSlices > 0)
-        {
-            jumbo+=1;
-            tomatoSlices -= 4;
-            cheeseSlices -= 1 ;
-        }  
-
-        if(cheeseSlices == 0 and tomatoSlices == 0)
-        {
-            ans.push_back(jumbo);
-            ans.push_back(small);
-        }
-        return ans;
-
+        if (tomatoSlices % 2 != 0) return {};
+        int jumbo = tomatoSlices / 2 - cheeseSlices;
+        int small = 2 * cheeseSlices - tomatoSlices / 2;
+        if (jumbo < 0 || small < 0) return {};
+        return {jumbo, small};
     }
 };
