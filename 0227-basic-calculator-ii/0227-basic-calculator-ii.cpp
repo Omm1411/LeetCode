c5958d05fd753a2e203c8104605acdc0c51492fe
Omm1411 <76055221+Omm1411@users.noreply.github.com>
2026-08-23 20:54:48 +0530

    Time: 5 ms (72.92%), Space: 15.2 MB (60.5%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -1,40 +1,53 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> st;
         char op = '+';
-        int i = 0 ;
-        while(i < s.size()){
-            if(s[i] == ' '){
-                 i++;
+        stack<int> st;
+        int i = 0;
+        while(i<s.size())
+        {
+            if(s[i]==' ')
+            {
+                i++;
             }
-            else if(s[i] == '/' || s[i] == '+' || s[i] == '*' || s[i] =='-'){
+            else if(s[i]=='+' or s[i]=='-' or s[i]=='/' or s[i]=='*')
+            {
                 op = s[i];
                 i++;
-            }else {
+            }
+            else
+            {
                 int num = 0;
-                while(i < s.size() && isdigit(s[i]) ){
-                    num = num*10 + (s[i] - '0');
+                while(i<s.size() and isdigit(s[i]))
+                {
+                    num = num*10 + (s[i]-'0');
                     i++;
                 }
-                if(op == '+'){
-                    st.push(num);
-                }else if( op == '-'){
-                    st.push(num*-1);
-                }else{
-                    int x = st.top();
-                    st.pop();
-                    (op == '*') ? st.push(num*x) : st.push(x/num);
-                }
+
+            
+            if(op == '+')
+            {
+                st.push(num);
+            }
+            else if(op == '-')
+            {
+                st.push(num*-1);
+            }
+            else
+            {
+                int x = st.top();
+                st.pop();
+                if(op == '*')st.push(num*x);
+                else st.push(x/num);
+            }
             }
         }
         int ans = 0;
-        while(!st.empty()){
-            ans +=st.top();
+        while(!st.empty())
+        {
+            ans+=st.top();
             st.pop();
         }
-
-return ans ;
-        
+        return ans;
     }
 };
